f8b222da683169f5520838b555feb21bd444b109
Alexander <sanekmakienko322@gmail.com>
2022-01-14 13:18:26 +0400

    Update readme

--- app/Exporters/EcbExporter.php
+++ app/Exporters/EcbExporter.php
@@ -15,7 +15,7 @@ class EcbExporter implements ExporterInterface
         foreach ($xml->getOriginalContent()->Cube->Cube->Cube as $item) {
             $rate = $item['rate'];
 
-            if (strtolower($mainCurrency) !== config('currencies.default')) {
+            if ($mainCurrency !== config('currencies.default')) {
                 $rate = app()->make(ConverterService::class)->convert($mainCurrency, config('currencies.default'), $item['rate']);
             }
 
